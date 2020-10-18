0f1c992e25cd527ea19b742f3a8fbb1c740aafbe
Prasad Kumkar <43164022+prasad-kumkar@users.noreply.github.com>
2020-10-19 00:56:33 +0530

    replaced src with logo link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./styles.css";
 import Wallet from "./Wallet/Wallet";
-import logo from "./assets/logo.PNG";
 
 export default function App() {
   return (
     <div className="App">
       <div className="header">
-        <img className="logo" src={logo} alt="logo"/>
+        <img className="logo" src="https://uploads.codesandbox.io/uploads/user/90c8d0d8-b495-4f1e-9f89-fe6e04efc17c/cU2P-logo.PNG" alt="logo"/>
       </div>
       <Wallet />
 
